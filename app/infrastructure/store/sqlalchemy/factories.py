7cec7b29f985b0fc7c69a2c13ffce711269db23f
mihey <daniil53891@yandex.com>
2023-02-28 21:56:24 +0300

    add sqlalchemy

---
+++ app/infrastructure/store/sqlalchemy/factories.py
@@ -0,0 +1,20 @@
+from typing import Callable
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+    AsyncSession,
+)
+
+
+def create_session_factory(url: str) -> Callable:
+    engine = create_async_engine(url, echo=True)
+
+    async_session = async_sessionmaker(
+        engine, class_=AsyncSession, expire_on_commit=False
+    )
+
+    async def session_factory():
+        async with async_session() as session:
+            yield session
+
+    return session_factory
