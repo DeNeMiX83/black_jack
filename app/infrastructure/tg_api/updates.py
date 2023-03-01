d9f450c3fd44940523e91aac68244bc0878d6fdc
mihey <daniil53891@yandex.com>
2023-03-02 00:32:41 +0300

    add tg api poller

---
+++ app/infrastructure/tg_api/updates.py
@@ -0,0 +1,25 @@
+import asyncio
+from asyncio import Task
+from typing import Optional
+from app.infrastructure.tg_api import Poller, Handler
+
+
+class Updates:
+    def __init__(self, poller: Poller, handler: Handler):
+        self._poller = poller
+        self._handler = handler
+        self._is_running = False
+        self._poll_task: Optional[Task] = None
+
+    async def start(self):
+        self._is_running = True
+        await self.poll()
+
+    async def poll(self):
+        while self._is_running:
+            try:
+                updates = await self._poller.get_updates()
+                asyncio.create_task(self._handler.handle_updates(updates))
+            except Exception:
+                await asyncio.sleep(5)
+    
