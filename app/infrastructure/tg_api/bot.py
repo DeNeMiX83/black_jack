809122384244c009eaa30589049a037dbafe2532
mihey <daniil53891@yandex.com>
2023-03-04 14:11:11 +0300

    fix

--- app/infrastructure/tg_api/bot.py
+++ app/infrastructure/tg_api/bot.py
@@ -1,5 +1,7 @@
 from typing import Optional
 from aiohttp import ClientSession
+from app.infrastructure.tg_api.handler import Handler
+from app.infrastructure.tg_api.filter import Filter
 
 
 class TgBot():
@@ -7,6 +9,10 @@ class TgBot():
         self._token = token
         self._url = url
         self._session = ClientSession()
+        self._handlers: list[Handler] = []
+
+    async def get_handlers(self) -> list[Handler]:
+        return self._handlers
 
     async def send_message(self, chat_id, text) -> Optional[dict]:
         url = f"{self._url}sendMessage"
@@ -20,3 +26,12 @@ class TgBot():
             response.raise_for_status()
             data = await response.json()
             return data
+
+    def add_handler(self, *filters: list[Filter]):
+        def decorator(handler_func):
+            handler = Handler(handler_func, self._bot)
+            handler.add_filters(filters)
+            self._handlers.append(handler)
+            return handler_func
+        return decorator
+
--- app/infrastructure/tg_api/handler_update.py
+++ app/infrastructure/tg_api/handler_update.py
@@ -1,12 +1,9 @@
 from app.infrastructure.tg_api.dto import Update
-from app.infrastructure.tg_api.handler import Handler
-from app.infrastructure.tg_api.filter import Filter
 from app.infrastructure.tg_api.bot import TgBot
 
 
 class HandlerUpdate:
     def __init__(self, bot: TgBot):
-        self._handlers: list[Handler] = []
         self._bot = bot
 
     async def handle_updates(self, updates: list[Update]):
@@ -14,14 +11,6 @@ class HandlerUpdate:
             await self._handle_update(update)
 
     async def _handle_update(self, update: Update):
-        for handler in self._handlers:
+        for handler in await self._bot.get_handlers():
             if await handler.filter(update):
                 await handler.handle(update)
-
-    def add_handler(self, filters: list[Filter]):
-        def decorator(handler_func):
-            handler = Handler(handler_func, self._bot)
-            handler.add_filters(filters)
-            self._handlers.append(handler)
-            return handler_func
-        return decorator
