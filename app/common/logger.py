7e07db5b08f25992b9d5a81cda474d45a63016d6
mihey <daniil53891@yandex.com>
2023-03-16 00:21:47 +0300

    fix

--- app/common/logger.py
+++ app/common/logger.py
@@ -4,7 +4,7 @@ logger = logging.getLogger('my_logger')
 
 logger.setLevel(logging.INFO)
 
-file_handler = logging.FileHandler('logs/tg_bot.log')
+file_handler = logging.FileHandler('tg_bot.log')
 
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(formatter)
